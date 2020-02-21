b52fce012ac9b6bb358ef5ff1681b7449783da14
alectramell <alectramell@gmail.com>
2020-02-21 07:58:55 -0800

    install

---
+++ README.md
@@ -0,0 +1,1 @@
+# Windows App Installer Build v1.0
---
+++ setup.bat
@@ -0,0 +1,54 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFA5HSRa+GG6pDaET+NTy4fmTo0ERaPUob7PS17OyI+8d+UrocNgk1XU6
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFA5HSRa+GG6pDaET+NTy4fmTo0ERaPE6bJrLlLGWJYA=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+
+cls
+
+set APPNAME="MYAPP"
+
+cls
+
+title %APPNAME%-INSTALLER
+
+cls
+
+color 0F
+
+cls
+
+mkdir "C:\%APPNAME%"
+move "%cd%\www" "C:\%APPNAME%\www"
+move "%cd%\%APPNAME%.exe" "C:\%APPNAME%\%APPNAME%.exe"
+
+pause
